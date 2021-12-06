d5d1d483957d1b1bfe68110ed749f38be0b12efc
trajnishBrown <tarun_rajnish@brown.edu>
2021-12-05 19:18:26 -0500

    seq2seq model

---
+++ Tarun/seq.py
@@ -0,0 +1,100 @@
+import os
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Sequential, Model, Input
+from tensorflow.keras.layers import Reshape, Conv2D, PReLU, Flatten, Dense, Activation
+from tensorflow.keras.losses import MeanSquaredError
+
+import matplotlib
+matplotlib.use('Agg')
+
+
+class Seq_Model(tf.keras.Model):
+    def __init__(self, Par):
+        super(Seq_Model, self).__init__()
+        np.random.seed(23)
+        tf.random.set_seed(23)
+
+        # Defining some model parameters
+        self.latent_dim = 100
+
+        self.Par = Par
+
+        self.index_list = []
+        self.train_loss_list = []
+        self.val_loss_list = []
+
+        self.lr = 10**-4
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr)
+
+        # self.embedding_size = 128
+        self.window_size = 20
+
+        # self.encoder_embed = tf.keras.layers.Embedding(
+        #     150, self.embedding_size, input_length=self.window_size)
+        # self.decoder_embed = tf.keras.layers.Embedding(
+        #     150, self.embedding_size, input_length=self.window_size)
+
+        self.encoder = tf.keras.layers.GRU(
+            self.window_size, return_sequences=True, return_state=True)
+
+        self.decoder = tf.keras.layers.GRU(
+            self.window_size, return_sequences=True, return_state=True)
+
+        self.dense = tf.keras.layers.Dense(
+            self.latent_dim)
+
+    # @tf.function(jit_compile=True)
+    def call(self, X):
+        X = tf.convert_to_tensor(X)
+
+        # print("X shape: ", X.shape)
+
+        # encoder_embeddings = self.encoder_embed(X_func)
+
+        # print("encoder_embeddings shape: ", encoder_embeddings.shape)
+        # print("encoder_embeddings: ", encoder_embeddings[0, 0, 0])
+
+        # X = tf.reshape(X, [-1, self.window_size, 1])
+
+        print("X shape: ", X.shape)
+
+        whole_sequence_output, final_state = self.encoder(X)
+
+        print("encoder_output whole_sequence_output shape: ",
+              whole_sequence_output.shape)
+        print("encoder_output final_state shape: ", final_state.shape)
+
+        # decoder_embeddings = self.decoder_embed(y)
+
+        # print("decoder_embeddings shape: ", decoder_embeddings.shape)
+
+        decoder_output, _ = self.decoder(
+            whole_sequence_output, initial_state=final_state)
+
+        print("decoder_output shape: ", decoder_output.shape)
+
+        y_pred = self.dense(decoder_output)
+
+        print("y_pred shape: ", y_pred.shape)
+
+        return(y_pred)
+
+    def loss(self, y_pred, y_train):
+
+        #-------------------------------------------------------------#
+        # Total Loss
+        print("Shape y_pred: ", y_pred.shape)
+        print("Shape y_train: ", y_train.shape)
+
+        train_loss = tf.reduce_mean(tf.square(y_pred - y_train))
+
+        # train_loss = tf.reduce_sum(
+        #     tf.cast(tf.keras.losses.sparse_categorical_crossentropy(y_train, y_pred, from_logits=True), dtype=tf.float32))
+        #-------------------------------------------------------------#
+
+        print("train_loss: ", train_loss)
+
+        return([train_loss])
---
+++ Tarun/train_seq.py
@@ -0,0 +1,146 @@
+from traceback import print_tb
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import time
+from sklearn.preprocessing import StandardScaler
+from scipy.interpolate import interp1d
+
+from seq import Seq_Model
+
+
+def preprocess(dataset, m, npoints_output, is_test=False):
+    p = dataset['del_p']
+    t = dataset['t']
+    r = dataset['R']
+
+    # print('p shape:', p.shape)
+    # print('t shape:', t.shape)
+    # print('r shape:', r.shape)
+
+    # print('p:', p[0, :])
+    # print('t:', t[0])
+    # print('r:', r[0, :])
+
+    P = interp1d(t, p, kind='cubic')
+    R = interp1d(t, r, kind='cubic')
+
+    t_min = 0
+    t_max = 5 * 10**-4
+
+    X_func = P(np.linspace(t_min, t_max, m))  # [1500, m]
+    # X_loc  = np.linspace(t_min, t_max, npoints_output)[:, None] #[npoints_output,1]
+    y = R(np.linspace(t_min, t_max, m))  # [1500, npoints_output]
+
+    if is_test:
+        X_func = X_func[:50]
+        y = y[:50]
+
+    # print('X_func shape:', X_func.shape)
+    # print('y shape:', y.shape)
+
+    return X_func, y
+
+
+def normalize(X_func, y, Par):
+    X_func = (X_func - Par['p_mean'])/Par['p_std']
+    y = (y - Par['r_mean'])/Par['r_std']
+
+    return X_func.astype(np.float32), y.astype(np.float32)
+
+
+# @tf.function(jit_compile=True)
+def train(seq_model, X, y):
+    with tf.GradientTape() as tape:
+        y_hat = seq_model(X)
+        loss = seq_model.loss(y_hat, y)[0]
+
+    gradients = tape.gradient(loss, seq_model.trainable_variables)
+    seq_model.optimizer.apply_gradients(
+        zip(gradients, seq_model.trainable_variables))
+    return(loss)
+
+
+def main():
+    Par = {}
+    Par['address'] = 'seq'
+
+    train_dataset = np.load('data/0.1/res_1000.npz')
+    test_dataset = np.load('data/0.08/res_1000.npz')
+
+    m = 20
+    npoints_output = 200
+
+    X_train, y_train = preprocess(
+        train_dataset, m, npoints_output)
+    X_test, y_test = preprocess(
+        test_dataset, m, npoints_output, is_test=True)
+
+    X_train = np.reshape(X_train, [-1, m, 1])
+    X_test = np.reshape(X_test, [-1, m, 1])
+    y_train = np.reshape(y_train, [-1, m, 1])
+    y_test = np.reshape(y_test, [-1, m, 1])
+
+    Par['p_mean'] = np.mean(X_train)
+    Par['p_std'] = np.std(X_train)
+
+    Par['r_mean'] = np.mean(y_train)
+    Par['r_std'] = np.std(y_train)
+
+    X_train, y_train = normalize(X_train, y_train, Par)
+    X_test, y_test = normalize(X_test, y_test, Par)
+
+    print('X_func_train: ', X_train.shape, '\ny_train: ', y_train.shape)
+    print('X_func_test: ', X_test.shape, '\ny_test: ', y_test.shape)
+
+    seq_model = Seq_Model(Par)
+    n_epochs = 100000
+    batch_size = 150
+
+    print("Seq2Seq Training Begins")
+    begin_time = time.time()
+
+    for i in range(n_epochs+1):
+        for end in np.arange(batch_size, X_train.shape[0]+1, batch_size):
+            start = end - batch_size
+
+            print('Epoch: ', i, '\tBatch: ', start, '\t', end)
+
+            loss = train(
+                seq_model, X_train[start:end], y_train[start:end])
+
+            print('Loss: ', loss)
+
+        if i % 1000 == 0:
+            seq_model.save_weights(Par['address'] + "/model_"+str(i))
+
+            train_loss = loss.numpy()
+
+            y_hat = seq_model(X_test)
+
+            val_loss = np.mean((y_hat - y_test)**2)
+
+            print("epoch:" + str(i) + ", Train Loss:" + "{:.3e}".format(train_loss) + ", Val Loss:" + "{:.3e}".format(
+                val_loss) + ", elapsed time: " + str(int(time.time()-begin_time)) + "s")
+
+            seq_model.index_list.append(i)
+            seq_model.train_loss_list.append(train_loss)
+            seq_model.val_loss_list.append(val_loss)
+
+    # Convergence plot
+    index_list = seq_model.index_list
+    train_loss_list = seq_model.train_loss_list
+    val_loss_list = seq_model.val_loss_list
+
+    plt.plot(index_list, train_loss_list, label="train")
+    plt.plot(index_list, val_loss_list, label="val")
+    plt.legend()
+    plt.yscale('log')
+    plt.xlabel("Epoch", fontsize=18)
+    plt.ylabel("MSE", fontsize=18)
+    plt.savefig(Par["address"] + "/convergence.png")
+
+    print('Complete')
+
+
+main()
